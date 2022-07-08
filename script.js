628293457e61d6aa8da55c0c8c8e91e0bfae6111
ID <izaakduncan@outlook.com>
2022-07-08 06:35:44 -0700

    initial build

---
+++ script.js
@@ -0,0 +1,13 @@
+const generateButton = document.getElementById('generateButton')
+const roleSelect = document.getElementById('selectRoleID')
+const generatedChampion = document.getElementById('generatedChampion')
+const wikiLink = document.getElementById('wikiLink')
+
+generateButton.addEventListener('click', function() {
+    let roleSelection = `${roleSelect.value}`
+    let randomValueFromArrayLength = Math.floor(Math.random() * roles[roleSelection].length)
+    let championValue = `${roles[roleSelection][randomValueFromArrayLength]}`
+    generatedChampion.innerHTML = championValue
+    wikiLink.href = `https://leagueoflegends.fandom.com/wiki/${championValue}/LoL`
+    wikiLink.innerHTML = 'View Champion Wiki'
+})
