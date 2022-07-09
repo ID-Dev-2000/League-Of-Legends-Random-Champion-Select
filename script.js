015b18cfb4b518396bec56aa5660d67adcc61e49
ID <izaakduncan@outlook.com>
2022-07-08 19:44:32 -0700

    minor code cleanup

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const generatedChampion = document.getElementById('generatedChampion')
 const wikiLink = document.getElementById('wikiLink')
 
 generateButton.addEventListener('click', function() {
-    let roleSelection = `${roleSelect.value}`
+    let roleSelection = roleSelect.value
     let randomValueFromArrayLength = Math.floor(Math.random() * roles[roleSelection].length)
-    let championValue = `${roles[roleSelection][randomValueFromArrayLength]}`
+    let championValue = roles[roleSelection][randomValueFromArrayLength]
     generatedChampion.innerHTML = championValue
     wikiLink.href = `https://leagueoflegends.fandom.com/wiki/${championValue}/LoL`
     wikiLink.innerHTML = 'View Champion Wiki'
