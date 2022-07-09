e612f5c817efe638e2c5be5d5ea3aa5b414e2035
ID <izaakduncan@outlook.com>
2022-07-09 02:42:58 -0700

    visual refactor

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const generateButton = document.getElementById('generateButton')
 const roleSelect = document.getElementById('selectRoleID')
 const generatedChampion = document.getElementById('generatedChampion')
 const wikiLink = document.getElementById('wikiLink')
+const rollCount = document.getElementById('rollCount')
 
+let rollCountVariable = 0
 generateButton.addEventListener('click', function() {
+    rollCountVariable++
+    rollCount.innerHTML = rollCountVariable
     let roleSelection = roleSelect.value
     let randomValueFromArrayLength = Math.floor(Math.random() * roles[roleSelection].length)
     let championValue = roles[roleSelection][randomValueFromArrayLength]
     generatedChampion.innerHTML = championValue
     wikiLink.href = `https://leagueoflegends.fandom.com/wiki/${championValue}/LoL`
     wikiLink.innerHTML = 'View Champion Wiki'
+})
+
+roleSelect.addEventListener('change', function() {
+    rollCountVariable = 0
+    rollCount.innerHTML = rollCountVariable
 })
